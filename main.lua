7d901fdd70a913ba548325c05de90fda67d26f2b
Cooperrrrs <109444963+Cooperrrrs@users.noreply.github.com>
2024-07-23 05:30:45 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-local Players = game:GetService('Players')
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local workspace = game:GetService("Workspace")
 local player = Players.LocalPlayer
