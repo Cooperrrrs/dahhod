24e0624c9ba19a8cb7a8117ab96608f6ff6b66f9
Cooperrrrs <109444963+Cooperrrrs@users.noreply.github.com>
2024-07-23 20:29:30 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,14 @@
+-- Config
 
+_G.host = "devcoopr"
+_G.Alts = {
+    ["1"] = "ragebestestguyever",
+    ["2"] = "Gamer_yt413",
+    ["3"] = "Blowqepew",
+    ["4"] = "pollito_yt5530",
+    ["5"] = "MoozeDK",
+    ["6"] = "akaishaiki",
+    ["7"] = "LEYENGAMER_YT3"
+}
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Cooperrrrs/dahhod/main/main.lua"),true))()
