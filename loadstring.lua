c1c5d11de83fed7517ab34b4b9aba2959fb66fe7
Cooperrrrs <109444963+Cooperrrrs@users.noreply.github.com>
2024-07-26 02:57:37 -0400

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,7 +1,8 @@
 -- Config
 
-_G.host = "devcoopr"
-_G.alts = {
+_G.host = "devcoopr" -- your username
+_G.Pickup = "I have come to get you!" -- when your going to TP someone
+_G.alts = { -- All the accounts you want to control.
     ["1"] = "ragebestestguyever",
     ["2"] = "Gamer_yt413",
     ["3"] = "Blowqepew",
@@ -11,7 +12,7 @@ _G.alts = {
     ["7"] = "LEYENGAMER_YT3"
 }
 
-_G.codes = {
+_G.codes = { -- codes for redeemding
     "PENGUIN",
     "BLAZE",
     "RUBY",
@@ -19,5 +20,19 @@ _G.codes = {
     "TRADEME!",
     "DAUP",
 }
+
+--[
+-- bring (brings all the bots)
+-- drop (true or false)
+-- redeem codes (redeem all codes in table)
+-- msg string (sends a message with all the bots)
+-- bring small (brings all the bots into a small locaton)
+-- tp plr bot (brings the plr to u)
+-- tph plr bot (teleports u to the plr)
+-- hide bot or All(hides the bots)
+-- unhide bot or All  (un hides the bots)
+-- tpo plr plrtotp bot (teleports a player to another player)
+-- cmds shows all cmds
+--]
 
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Cooperrrrs/dahhod/main/main.lua"),true))()
