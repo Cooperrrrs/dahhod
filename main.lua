853126c7dce401e985a2fc706fba6b4a330ebb9c
Cooperrrrs <109444963+Cooperrrrs@users.noreply.github.com>
2024-07-25 22:49:47 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -183,7 +183,7 @@ function killnbring(target, acc)
 				wait(0.5)
 				player.Character.HumanoidRootPart.CFrame = RTARGET.Character.HumanoidRootPart.CFrame
 				wait(0.2)
-				player.Character.HumanoidRootPart.CFrame = RTARGET.Character.HumanoidRootPart.CFrame
+				player.Character.HumanoidRootPart.CFrame = RTARGET.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, -5)
 				wait(0.3)
 				local args = {
 					[1] = "Grabbing",
@@ -344,7 +344,7 @@ function tpo(plrtobring, plrtogoto, botgoing)
 				wait(0.5)
 				player.Character.HumanoidRootPart.CFrame = TPTARGET.Character.HumanoidRootPart.CFrame
 				wait(0.2)
-				player.Character.HumanoidRootPart.CFrame = TPTARGET.Character.HumanoidRootPart.CFrame
+				player.Character.HumanoidRootPart.CFrame = TPTARGET.Character.HumanoidRootPart.CFrame * CFrame.new(0, 0, -5)
 				wait(0.3)
 				local args = {
 					[1] = "Grabbing",
